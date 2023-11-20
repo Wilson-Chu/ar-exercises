8f8e7444b9f732a285fdbdcd2df25a4c7a9a6ad2
Wilson Chu <wilson1.chu@gmail.com>
2023-11-20 18:47:03 -0500

    complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -18,3 +18,9 @@ puts "----------"
  
 Store.has_many(:employees)
 Employee.belongs_to(:store)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Alice", last_name: "Wonderland", hourly_rate: 15)
+@store2.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 50)
+@store2.employees.create(first_name: "Bob", last_name: "Burger", hourly_rate: 20)
+@store2.employees.create(first_name: "Petra", last_name: "Wallace", hourly_rate: 100)
